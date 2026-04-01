a6536f8d1d891b2c57aff2f187f9d49156229920
CCrisix <omgitzrllyu@gmail.com>
2026-04-01 04:29:45 -0400

    added main file for easier usage

--- main.py
+++ main.py
@@ -2,24 +2,29 @@ from engine import SudokuEngine
 import time
 import pyautogui
 
-puzzle = input("Puzzle: ")
-engine = SudokuEngine(puzzle)
-engine.solve()
-answer = engine.board
-rows = len(answer)
-cols = len(answer[0])
+def main():
+    puzzle = input("Puzzle: ")
+    engine = SudokuEngine(puzzle)
+    engine.solve()
+    answer = engine.board
+    rows = len(answer)
+    cols = len(answer[0])
 
-time.sleep(2)
+    time.sleep(5)
 
-for row in range(rows):
-    for col in range(cols):
-        pyautogui.press(str(answer[row][col]))
+    for row in range(rows):
+        for col in range(cols):
+            pyautogui.press(str(answer[row][col]))
 
-        if col < cols - 1:
-            pyautogui.press('right')
+            if col < cols - 1:
+                pyautogui.press('right')
 
-    if row < rows - 1:
-        for cell in range(cols - 1):
-            pyautogui.press('left')
+        if row < rows - 1:
+            for cell in range(cols - 1):
+                pyautogui.press('left')
 
-        pyautogui.press('down')
+            pyautogui.press('down')
+
+if __name__ == '__main__':
+    main()
+
